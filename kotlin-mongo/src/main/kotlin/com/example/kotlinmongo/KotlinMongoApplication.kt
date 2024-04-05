1b6be3c5b930c0ad78ea71bbd09ef83aa98b8d5c
lolmageap <ekxk1234@naver.com>
2024-04-05 23:37:18 +0900

    add spring module and

---
+++ kotlin-mongo/src/main/kotlin/com/example/kotlinmongo/KotlinMongoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinmongo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinMongoApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinMongoApplication>(*args)
+}
