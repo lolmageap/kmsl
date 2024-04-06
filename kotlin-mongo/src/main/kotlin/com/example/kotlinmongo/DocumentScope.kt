45480d63aec1729c96c2759940ef4ce0d3288f1c
lolmageap <ekxk1234@naver.com>
2024-04-06 23:51:13 +0900

    fix generic type

--- kotlin-mongo/src/main/kotlin/com/example/kotlinmongo/DocumentScope.kt
+++ kotlin-mongo/src/main/kotlin/com/example/kotlinmongo/DocumentScope.kt
@@ -1,12 +1,13 @@
 package com.example.kotlinmongo
 
 import org.bson.Document
+import kotlin.reflect.full.createInstance
 
-inline fun <reified T> document(
+inline fun <reified T: Any> document(
     function: Document.(T) -> Unit,
 ): Document {
     return Document().apply {
-        val instance = T::class.java.getDeclaredConstructor().newInstance()
+        val instance = T::class.createInstance()
         function(instance)
     }
 }
