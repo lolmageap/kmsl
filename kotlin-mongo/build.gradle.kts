c586978675a1fa85412d61838cde96882618fadc
lolmageap <ekxk1234@naver.com>
2024-04-07 17:41:07 +0900

    add test dependency

--- kotlin-mongo/build.gradle.kts
+++ kotlin-mongo/build.gradle.kts
@@ -23,7 +23,12 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
+	testImplementation("io.kotest:kotest-assertions-core:5.7.2")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+	testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:2.0.2")
 }
 
 tasks.withType<KotlinCompile> {
