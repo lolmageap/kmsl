5f83371bc7ee2154490e1e827d8c821a933320a8
lolmageap <ekxk1234@naver.com>
2024-04-07 17:39:52 +0900

    add properties

--- kotlin-mongo/src/main/kotlin/com/example/kotlinmongo/Author.kt
+++ kotlin-mongo/src/main/kotlin/com/example/kotlinmongo/Author.kt
@@ -1,7 +1,9 @@
 package com.example.kotlinmongo
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.DBRef
 import org.springframework.data.mongodb.core.mapping.Document
+import java.io.Serializable
 
 @Document
 class Author(
@@ -9,5 +11,25 @@ class Author(
     val id: String,
     val name: String,
     val age: Int,
+    val weight: Double,
+    val height: Float,
+
+    @DBRef
     val books: List<Book>,
+
+    val address: Address,
+    val company: Company,
 )
+
+@Document
+data class Address(
+    val city: String,
+    val street: String,
+    val zipcode: String,
+): Serializable
+
+@Document
+data class Company(
+    val name: String,
+    val ceo: String,
+): Serializable
