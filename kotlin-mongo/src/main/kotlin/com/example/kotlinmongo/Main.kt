efeeea182b48b92d1e615b87b969e8a32c02cd68
lolmageap <ekxk1234@naver.com>
2024-04-08 22:24:03 +0900

    fix sample code

--- kotlin-mongo/src/main/kotlin/com/example/kotlinmongo/Main.kt
+++ kotlin-mongo/src/main/kotlin/com/example/kotlinmongo/Main.kt
@@ -2,25 +2,10 @@ package com.example.kotlinmongo
 
 fun main() {
     val document = document {
-        field(Author::weight) gteAndLt (70.0 to 80.0)
-        field(Author::height) `in` listOf(170.0f, 180.0f)
+        field(Author::name) eq "John"
+        field(Author::age) gt 18
 
-        field(Address::street) gt "5th Avenue"
-        field(Address::city) between ("New York" to "Los Angeles")
-        field(Address::zipcode) contains "93"
-
-        field(Company::name) nin listOf("Apple", "Google")
-        field(Company::ceo) startsWith "John"
-
-        orBuilder {
-            or {
-                field(Author::name) eq "Jane Doe"
-            }
-
-            or {
-                field(Author::age) `in` listOf(30, 40)
-            }
-        }
+        groupBy(Author::name).count()
     }
 
     println(document)
