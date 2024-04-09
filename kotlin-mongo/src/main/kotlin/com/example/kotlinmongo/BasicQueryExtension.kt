badd526af54241a97c7c8d43dc2f5b84536ea37e
lolmageap <ekxk1234@naver.com>
2024-04-09 23:29:06 +0900

    create basic query extension

---
+++ kotlin-mongo/src/main/kotlin/com/example/kotlinmongo/BasicQueryExtension.kt
@@ -0,0 +1,22 @@
+package com.example.kotlinmongo
+
+import org.bson.Document
+import org.springframework.data.mongodb.core.query.BasicQuery
+import kotlin.reflect.KProperty1
+
+fun <T, R> BasicQuery.groupBy(
+    key: KProperty1<T, R>,
+): Group<T, R> {
+    return Group(key, this.queryObject)
+}
+
+fun BasicQuery.groupBy(): EmptyGroup {
+    return EmptyGroup(this.queryObject)
+}
+
+fun BasicQuery.where(
+    document: Document.() -> Document,
+): BasicQuery {
+    val queryObject = this.queryObject
+    return BasicQuery(document.invoke(queryObject))
+}
