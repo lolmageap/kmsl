d933b6a002796c356f1de1ab5bae37dd1bcec76e
lolmageap <ekxk1234@naver.com>
2024-04-11 22:23:01 +0900

    fix document scope test

--- kotlin-mongo/src/test/kotlin/com/example/kotlinmongo/DocumentScopeTest.kt
+++ kotlin-mongo/src/test/kotlin/com/example/kotlinmongo/DocumentScopeTest.kt
@@ -5,7 +5,6 @@ import io.kotest.matchers.shouldBe
 import org.springframework.data.mongodb.core.query.BasicQuery
 
 class DocumentScopeTest : StringSpec({
-
     "document scope 내에서 field 를 사용 하면 BasicQuery 로 변환 된다." {
         val document = document {
             field(Author::name) eq "John"
@@ -23,69 +22,5 @@ class DocumentScopeTest : StringSpec({
 
         document shouldBe BasicQuery("{ \"name\" : \"Doe\" }")
     }
-
-    "document scope 내에서 동일한 field 를 중복해서 사용 하려면 and scope 에 add 로 각각 사용 해야 한다." {
-        val document = document {
-            andScope {
-                and { field(Author::name) eq "John" }
-                and { field(Author::name) eq "Doe" }
-            }
-        }
-
-        document shouldBe BasicQuery("{ \"\$and\" : [ { \"name\" : \"John\" }, { \"name\" : \"Doe\" } ] }")
-    }
-
-    "document scope 내에서 동일한 field 를 중복해서 or 연산을 사용 하려면 or scope 에 add 로 각각 사용 해야 한다." {
-        val document = document {
-            orScope {
-                or { field(Author::name) eq "John" }
-                or { field(Author::name) eq "Doe" }
-            }
-        }
-
-        document shouldBe BasicQuery("{ \"\$or\" : [ { \"name\" : \"John\" }, { \"name\" : \"Doe\" } ] }")
-    }
-
-    "or scope 내에서 and scope 를 사용하면 and 조건으로 연결된다." {
-        val document = document {
-            orScope {
-                or {
-                    field(Author::name) eq "John"
-                    field(Author::age) eq 18
-                }
-            }
-        }
-
-        document shouldBe BasicQuery("{ \"\$or\" : [ { \"name\" : \"John\", \"age\" : 18 } ] }")
-    }
-
-    "or scope 내에서 or 를 여러번 사용하면 or 조건으로 연결된다." {
-        val document = document {
-            orScope {
-                or {
-                    field(Author::name) eq "John"
-                    field(Author::age) eq 18
-                }
-                or {
-                    field(Author::name) eq "Jane"
-                    field(Author::age) eq 20
-                }
-            }
-        }
-
-        document shouldBe BasicQuery("{ \"\$or\" : [ { \"name\" : \"John\", \"age\" : 18 }, { \"name\" : \"Jane\", \"age\" : 20 } ] }")
-    }
-
-    "or scope 내에 하나의 add 에 동일한 field 를 여러번 사용하면 마지막 값으로 덮어 씌워진다." {
-        val document = document {
-            orScope {
-                or {
-                    field(Author::name) eq "John"
-                    field(Author::name) eq "Doe"
-                }
-            }
-        }
-
-        document shouldBe BasicQuery("{ \"\$or\" : [ { \"name\" : \"Doe\" } ] }")
-    }
+    // TIP : 이 문제를 해결 하기 위해서 OperatorBuilder 클래스 를 사용 해야 합니다.
 })
