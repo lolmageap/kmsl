9b8d728126d503d95739d4344d01c41528e5318c
lolmageap <ekxk1234@naver.com>
2024-05-25 22:30:59 +0900

    remove unused clazz and refactor document scope initialize

--- src/main/kotlin/com/example/kotlinmongo/extension/DocumentScope.kt
+++ src/main/kotlin/com/example/kotlinmongo/extension/DocumentScope.kt
@@ -1,44 +1,20 @@
 package com.example.kotlinmongo.extension
 
-import com.example.kotlinmongo.clazz.*
+import com.example.kotlinmongo.clazz.DocumentOperatorBuilder
+import com.example.kotlinmongo.clazz.Field
 import org.bson.Document
 import org.springframework.data.mongodb.core.query.BasicQuery
 import kotlin.reflect.KProperty1
 
 fun document(
-    rootOperatorType: RootOperatorType = RootOperatorType.AND,
     function: DocumentOperatorBuilder.() -> Unit,
 ): BasicQuery {
     val document = Document()
 
-    val documentOperatorBuilder = DocumentOperatorBuilder(
-        document = document,
-        rootOperatorType = rootOperatorType,
-        function = function,
-    ).run()
+    val documentOperatorBuilder = DocumentOperatorBuilder(document, function)
 
     return BasicQuery(
         documentOperatorBuilder.toJson()
-    )
-}
-
-fun orOperator(
-    function: OrOperatorBuilder.() -> Unit,
-): BasicQuery {
-    val document = Document()
-    val orOperatorBuilder = OrOperatorBuilder(document, function).run()
-    return BasicQuery(
-        orOperatorBuilder.toJson()
-    )
-}
-
-fun andOperator(
-    function: AndOperatorBuilder.() -> Unit,
-): BasicQuery {
-    val document = Document()
-    val orOperatorBuilder = AndOperatorBuilder(document, function).run()
-    return BasicQuery(
-        orOperatorBuilder.toJson()
     )
 }
 
--- src/test/kotlin/com/example/kotlinmongo/DocumentScopeTest.kt
+++ src/test/kotlin/com/example/kotlinmongo/DocumentScopeTest.kt
@@ -9,20 +9,12 @@ import org.springframework.data.mongodb.core.query.BasicQuery
 class DocumentScopeTest : StringSpec({
     "document scope 내에서 field 를 사용 하면 BasicQuery 로 변환 된다." {
         val document = document {
-            field(Author::name) eq "John"
-            field(Author::age) eq 18
+            and(
+                { field(Author::name) eq "John" },
+                { field(Author::age) eq 18 },
+            )
         }
 
         document shouldBe BasicQuery("{ \"name\" : \"John\", \"age\" : 18 }")
     }
-
-    "document scope 내에서 동일한 field 를 여러번 사용 하면 마지막에 사용된 값으로 덮어 씌워진다." {
-        val document = document {
-            field(Author::name) eq "John"
-            field(Author::name) eq "Doe"
-        }
-
-        document shouldBe BasicQuery("{ \"name\" : \"Doe\" }")
-    }
-    // TIP : 이 문제를 해결 하기 위해서 OperatorBuilder 클래스 를 사용 해야 합니다.
 })
