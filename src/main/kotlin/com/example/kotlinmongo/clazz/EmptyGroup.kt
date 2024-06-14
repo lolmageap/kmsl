dc45902979f18ff362a6ac1074ce7382eb1bb1cb
lolmageap <ekxk1234@naver.com>
2024-06-14 23:40:14 +0900

    add aggregate function to group

--- src/main/kotlin/com/example/kotlinmongo/clazz/EmptyGroup.kt
+++ src/main/kotlin/com/example/kotlinmongo/clazz/EmptyGroup.kt
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.core.aggregation.Aggregation
 import org.springframework.data.mongodb.core.aggregation.AggregationExpression
 import org.springframework.data.mongodb.core.aggregation.MatchOperation
 import org.springframework.data.mongodb.core.query.Criteria
+import kotlin.reflect.KClass
 
 class EmptyGroup(
     private val document: Document,
@@ -12,28 +13,72 @@ class EmptyGroup(
     fun sumOf(
         alias: String = "total",
         sumField: Document.() -> Field<*, *>,
-    ): Aggregation {
-        val matchStage = matchOperation()
-        return Aggregation.newAggregation(
-            matchStage,
-            Aggregation.group().sum(sumField.invoke(Document()).key.name).`as`(alias)
-        )
-    }
-
-    fun sumOfNumber(
-        alias: String = "total",
-        sumField: Document.() -> Field<*, *>,
+        type: KClass<*> = Long::class,
     ): Aggregation {
         val fieldName = sumField.invoke(Document()).key.name
-        val toIntExpression = AggregationExpression {
-            Document("\$toLong", "\$$fieldName")
+        val expression = AggregationExpression {
+            Document(MongoAggregateTypeFactory.exchange(type), "\$$fieldName")
         }
 
         val matchStage = matchOperation()
 
         return Aggregation.newAggregation(
             matchStage,
-            Aggregation.group().sum(toIntExpression).`as`(alias)
+            Aggregation.group().sum(expression).`as`(alias)
+        )
+    }
+
+    fun avgOf(
+        alias: String = "avg",
+        avgField: Document.() -> Field<*, *>,
+        type: KClass<*> = Long::class,
+    ): Aggregation {
+        val fieldName = avgField.invoke(Document()).key.name
+        val expression = AggregationExpression {
+            Document(MongoAggregateTypeFactory.exchange(type), "\$$fieldName")
+        }
+
+        val matchStage = matchOperation()
+
+        return Aggregation.newAggregation(
+            matchStage,
+            Aggregation.group().avg(expression).`as`(alias)
+        )
+    }
+
+    fun maxOf(
+        alias: String = "max",
+        maxField: Document.() -> Field<*, *>,
+        type: KClass<*> = Long::class,
+    ): Aggregation {
+        val fieldName = maxField.invoke(Document()).key.name
+        val expression = AggregationExpression {
+            Document(MongoAggregateTypeFactory.exchange(type), "\$$fieldName")
+        }
+
+        val matchStage = matchOperation()
+
+        return Aggregation.newAggregation(
+            matchStage,
+            Aggregation.group().max(expression).`as`(alias)
+        )
+    }
+
+    fun minOf(
+        alias: String = "min",
+        minField: Document.() -> Field<*, *>,
+        type: KClass<*> = Long::class,
+    ): Aggregation {
+        val fieldName = minField.invoke(Document()).key.name
+        val expression = AggregationExpression {
+            Document(MongoAggregateTypeFactory.exchange(type), "\$$fieldName")
+        }
+
+        val matchStage = matchOperation()
+
+        return Aggregation.newAggregation(
+            matchStage,
+            Aggregation.group().min(expression).`as`(alias)
         )
     }
 
--- src/main/kotlin/com/example/kotlinmongo/clazz/Group.kt
+++ src/main/kotlin/com/example/kotlinmongo/clazz/Group.kt
@@ -2,12 +2,16 @@ package com.example.kotlinmongo.clazz
 
 import org.bson.Document
 import org.springframework.data.mongodb.core.aggregation.Aggregation
+import org.springframework.data.mongodb.core.aggregation.AggregationExpression
 import org.springframework.data.mongodb.core.aggregation.MatchOperation
 import org.springframework.data.mongodb.core.query.Criteria
+import kotlin.reflect.KClass
 import kotlin.reflect.KProperty1
 
-// side effect 가 발생하지 않으려면 Group By 는 마지막 줄에 위치 시키는 것이 좋습니다.
-// grouping 을 할때 내부적으로 matchOperation 을 사용하기 때문에...
+/**
+ * grouping 을 할때 내부적으로 matchOperation 을 사용합니다.
+ * side effect 가 발생하지 않으려면 Group By 는 마지막 줄에 위치 시키는 것이 좋습니다.
+ */
 class Group<T, R>(
     private val key: KProperty1<T, R>,
     private val document: Document,
@@ -15,13 +19,72 @@ class Group<T, R>(
     fun sumOf(
         alias: String = "total",
         sumField: Document.() -> Field<T, *>,
+        type: KClass<*> = Long::class,
     ): Aggregation {
+        val fieldName = sumField.invoke(Document()).key.name
+        val expression = AggregationExpression {
+            Document(MongoAggregateTypeFactory.exchange(type), "\$$fieldName")
+        }
+
         val matchStage = matchOperation()
+
         return Aggregation.newAggregation(
             matchStage,
-            Aggregation.group(key.name).sum(
-                sumField.invoke(Document()).key.name
-            ).`as`(alias)
+            Aggregation.group(key.name).sum(expression).`as`(alias)
+        )
+    }
+
+    fun avgOf(
+        alias: String = "avg",
+        avgField: Document.() -> Field<T, *>,
+        type: KClass<*> = Long::class,
+    ): Aggregation {
+        val fieldName = avgField.invoke(Document()).key.name
+        val expression = AggregationExpression {
+            Document(MongoAggregateTypeFactory.exchange(type), "\$$fieldName")
+        }
+
+        val matchStage = matchOperation()
+
+        return Aggregation.newAggregation(
+            matchStage,
+            Aggregation.group(key.name).avg(expression).`as`(alias)
+        )
+    }
+
+    fun maxOf(
+        alias: String = "max",
+        maxField: Document.() -> Field<T, *>,
+        type: KClass<*> = Long::class,
+    ): Aggregation {
+        val fieldName = maxField.invoke(Document()).key.name
+        val expression = AggregationExpression {
+            Document(MongoAggregateTypeFactory.exchange(type), "\$$fieldName")
+        }
+
+        val matchStage = matchOperation()
+
+        return Aggregation.newAggregation(
+            matchStage,
+            Aggregation.group(key.name).max(expression).`as`(alias)
+        )
+    }
+
+    fun minOf(
+        alias: String = "min",
+        minField: Document.() -> Field<T, *>,
+        type: KClass<*> = Long::class,
+    ): Aggregation {
+        val fieldName = minField.invoke(Document()).key.name
+        val expression = AggregationExpression {
+            Document(MongoAggregateTypeFactory.exchange(type), "\$$fieldName")
+        }
+
+        val matchStage = matchOperation()
+
+        return Aggregation.newAggregation(
+            matchStage,
+            Aggregation.group(key.name).min(expression).`as`(alias)
         )
     }
 
