b96a6ac2800656e25cb6c2b023ae7473b3a96e60
lolmageap <ekxk1234@naver.com>
2024-08-01 19:59:34 +0900

    add jakarta persistence api dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
