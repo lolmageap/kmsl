90a06832b9786fa6661841bf2cbe655936204d15
lolmageap <ekxk1234@naver.com>
2024-08-04 21:46:55 +0900

    fix to id type and default collection name

--- src/main/kotlin/com/example/kotlinmongo/entity/Author.kt
+++ src/main/kotlin/com/example/kotlinmongo/entity/Author.kt
@@ -1,43 +1,21 @@
 package com.example.kotlinmongo.entity
 
 import jakarta.persistence.Id
-import org.springframework.data.mongodb.core.mapping.DBRef
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.mongodb.core.mapping.Field
-import java.io.Serializable
 
-@Document
+@Document(collection = "author")
 data class Author(
     @Id @Field("_id")
-    val id: String,
+    val id: String? = null,
     val name: String,
     val age: Int,
     val weight: Double,
     val height: Float,
     val status: Status,
-
-    @DBRef
-    val books: List<Book>,
-
-    val address: Address,
-    val company: Company,
-    val birthday: String,
 )
 
 enum class Status {
     ACTIVE,
     INACTIVE,
 }
-
-@Document
-data class Address(
-    val city: String,
-    val street: String,
-    val zipcode: String,
-): Serializable
-
-@Document
-data class Company(
-    val name: String,
-    val ceo: String,
-): Serializable
