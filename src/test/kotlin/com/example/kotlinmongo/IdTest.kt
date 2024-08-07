eb873db573585b9008981584ff5d5a5ed38bf80b
lolmageap <ekxk1234@naver.com>
2024-08-07 21:10:15 +0900

    create get id test

---
+++ src/test/kotlin/com/example/kotlinmongo/IdTest.kt
@@ -0,0 +1,38 @@
+package com.example.kotlinmongo
+
+import com.example.kotlinmongo.collection.Author
+import com.example.kotlinmongo.collection.Status
+import com.example.kotlinmongo.extension.document
+import com.example.kotlinmongo.extension.field
+import com.example.kotlinmongo.extension.find
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.mongodb.core.MongoTemplate
+
+@SpringBootTest
+class IdTest(
+    private val mongoTemplate: MongoTemplate,
+): StringSpec({
+    "id 값으로 조회" {
+        val author = mongoTemplate.insert(
+            Author(
+                name = "Test",
+                age = 100,
+                weight = 170.0,
+                height = 70f,
+                status = Status.INACTIVE,
+                books = emptyList(),
+            )
+        )
+
+        val document = document {
+            and(
+                { field(Author::id) eq author.id },
+            )
+        }
+
+        val result = mongoTemplate.find(document, Author::class).first()
+        result shouldBe author
+    }
+})
