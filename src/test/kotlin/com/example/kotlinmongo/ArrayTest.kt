b8515f8f5d41e15c79aac4813bfef094fee31f99
lolmageap <ekxk1234@naver.com>
2024-08-12 01:12:15 +0900

    add elem match function test

--- src/test/kotlin/com/example/kotlinmongo/ArrayTest.kt
+++ src/test/kotlin/com/example/kotlinmongo/ArrayTest.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinmongo
 
+import com.example.kotlinmongo.clazz.embeddedDocument
 import com.example.kotlinmongo.collection.Author
 import com.example.kotlinmongo.collection.Book
 import com.example.kotlinmongo.collection.Status.ACTIVE
@@ -9,6 +10,7 @@ import com.example.kotlinmongo.extension.field
 import com.example.kotlinmongo.extension.find
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.mongodb.core.MongoTemplate
@@ -106,6 +108,20 @@ class ArrayTest(
         document shouldBe BasicQuery("{ \"\$and\" : [{ \"books\" : {\"\$in\" : [\"book1\"]}}]}")
         val author = mongoTemplate.find(document, Author::class).first()
         author.books shouldBe listOf("book1", "book2")
+    }
+
+    "배열 내부 오브젝트 필드에 대한 연산 테스트" {
+        val document = document {
+            embeddedDocument(Author::books).elemMatch({
+                or(
+                    { field(Book::title) eq "book1" },
+                )
+            })
+        }
+
+        val youngAuthor = mongoTemplate.find(document, Author::class).first()
+        val titles = youngAuthor.books.map { it.title }
+        assert(titles == mutableListOf("book1", "book2"))
     }
 })
 
