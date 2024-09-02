e35819366b7af110956686ffd33f5966dd96015c
lolmageap <ekxk1234@naver.com>
2024-09-02 23:42:17 +0900

    fix test to group by function and aggregation

--- src/test/kotlin/com/example/kotlinmongo/IdTest.kt
+++ src/test/kotlin/com/example/kotlinmongo/IdTest.kt
@@ -4,9 +4,8 @@ import com.example.kotlinmongo.collection.Author
 import com.example.kotlinmongo.collection.Status
 import com.example.kotlinmongo.extension.document
 import com.example.kotlinmongo.extension.field
-import com.example.kotlinmongo.extension.find
+import com.example.kotlinmongo.extension.group
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.shouldBe
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.mongodb.core.MongoTemplate
 
@@ -28,9 +27,21 @@ class IdTest(
 
         val document = document {
             field(Author::id) eq author.id
+        } group {
+            field(Author::status) and field(Author::age)
+        } sum {
+            field(Author::age) alias "total"
+        } avg {
+            field(Author::weight) alias "avg"
+        } max {
+            field(Author::height) alias "max"
+        } min {
+            field(Author::height) alias "min"
         }
 
-        val result = mongoTemplate.find(document, Author::class).first()
-        result shouldBe author
+        val aggregation = document.toAggregation()
+
+        val result = mongoTemplate.aggregate(aggregation, Author::class.java, Map::class.java).uniqueMappedResult
+        println("result = $result")
     }
 })
