15aed46e13e2aeb81a2f599f927b58d07b66db4e
lolmageap <ekxk1234@naver.com>
2024-09-03 23:49:41 +0900

    test to empty group aggregate function

--- src/test/kotlin/com/example/kotlinmongo/IdTest.kt
+++ src/test/kotlin/com/example/kotlinmongo/IdTest.kt
@@ -2,9 +2,8 @@ package com.example.kotlinmongo
 
 import com.example.kotlinmongo.collection.Author
 import com.example.kotlinmongo.collection.Status
-import com.example.kotlinmongo.extension.document
-import com.example.kotlinmongo.extension.field
-import com.example.kotlinmongo.extension.group
+import com.example.kotlinmongo.extension.*
+import com.example.kotlinmongo.extension.aggregate
 import io.kotest.core.spec.style.StringSpec
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.mongodb.core.MongoTemplate
@@ -30,8 +29,8 @@ class IdTest(
         } group {
             field(Author::status) and field(Author::age)
         } sum {
-            field(Author::age) alias "total"
-        } avg {
+            field(Author::age) type Double::class alias "total"
+        } average {
             field(Author::weight) alias "avg"
         } max {
             field(Author::height) alias "max"
@@ -39,9 +38,43 @@ class IdTest(
             field(Author::height) alias "min"
         }
 
-        val aggregation = document.toAggregation()
+        val map = mongoTemplate.aggregate(document, Author::class)
+        println("map: $map")
+        println(map["total"])
+        println(map["avg"])
+        println(map["max"])
+        println(map["min"])
+    }
 
-        val result = mongoTemplate.aggregate(aggregation, Author::class.java, Map::class.java).uniqueMappedResult
-        println("result = $result")
+    "id 값으로 조회2" {
+        val author = mongoTemplate.insert(
+            Author.of(
+                name = "Test",
+                age = 100,
+                weight = 170.0,
+                height = 70f,
+                status = Status.RETIREMENT,
+                books = mutableListOf(),
+            )
+        )
+
+        val document = document {
+            field(Author::id) eq author.id
+        } sum {
+            field(Author::age) alias "total"
+        } average {
+            field(Author::weight) alias "avg"
+        } max {
+            field(Author::height) alias "max"
+        } min {
+            field(Author::height) alias "min"
+        }
+
+        val map = mongoTemplate.aggregate(document, Author::class)
+        println("map: $map")
+        println(map["total"])
+        println(map["avg"])
+        println(map["max"])
+        println(map["min"])
     }
 })
