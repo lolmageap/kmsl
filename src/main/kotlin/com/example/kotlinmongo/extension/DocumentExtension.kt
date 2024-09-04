dfef80cfc77ca3f3a648db783ea17017ec3142d2
lolmageap <ekxk1234@naver.com>
2024-09-04 20:46:32 +0900

    separate file to match operation

---
+++ src/main/kotlin/com/example/kotlinmongo/extension/DocumentExtension.kt
@@ -0,0 +1,14 @@
+package com.example.kotlinmongo.extension
+
+import org.bson.Document
+import org.springframework.data.mongodb.core.aggregation.Aggregation
+import org.springframework.data.mongodb.core.aggregation.MatchOperation
+import org.springframework.data.mongodb.core.query.Criteria
+
+fun Document.matchOperation(): MatchOperation {
+    val criteria = Criteria()
+    for ((key, value) in this) {
+        criteria.and(key).`is`(value)
+    }
+    return Aggregation.match(criteria)
+}
