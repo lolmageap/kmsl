bc4c0b4b3343322d1f4265fd2b5fc18ab5427b37
lolmageap <ekxk1234@naver.com>
2024-09-04 20:48:07 +0900

    fix to order by test

--- src/test/kotlin/com/example/kotlinmongo/OrderByTest.kt
+++ src/test/kotlin/com/example/kotlinmongo/OrderByTest.kt
@@ -1,20 +1,24 @@
 package com.example.kotlinmongo
 
+import com.example.kotlinmongo.clazz.field
 import com.example.kotlinmongo.collection.Author
 import com.example.kotlinmongo.extension.RootDocumentOperator.OR
 import com.example.kotlinmongo.extension.document
-import com.example.kotlinmongo.extension.field
-import com.example.kotlinmongo.extension.orderBy
+import com.example.kotlinmongo.extension.order
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import org.bson.Document
+import org.springframework.data.domain.Sort.Direction.ASC
+import org.springframework.data.domain.Sort.Direction.DESC
 
 class OrderByTest : StringSpec({
     "단일 orderBy 정렬 테스트" {
         val result = document(OR) {
             field(Author::name) eq "John"
             field(Author::age) eq 18
-        }.orderBy(Author::name).desc()
+        } order {
+            field(Author::name) by DESC
+        }
 
         result.sortObject shouldBe Document("name", -1)
     }
@@ -23,8 +27,10 @@ class OrderByTest : StringSpec({
         val result = document {
             field(Author::name) eq "John"
             field(Author::age) eq 18
-        }.orderBy(Author::name).desc()
-            .orderBy(Author::age).asc()
+        } order {
+            field(Author::name) by DESC
+            field(Author::age) by ASC
+        }
 
         result.sortObject shouldBe Document("name", -1).append("age", 1)
     }
