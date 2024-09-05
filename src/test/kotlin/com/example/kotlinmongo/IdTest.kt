6bae9fd45183b4325df90bfff60e29e002090ae9
lolmageap <ekxk1234@naver.com>
2024-09-05 23:34:04 +0900

    add test to embedded document and elemMatch

--- src/test/kotlin/com/example/kotlinmongo/IdTest.kt
+++ src/test/kotlin/com/example/kotlinmongo/IdTest.kt
@@ -2,6 +2,7 @@ package com.example.kotlinmongo
 
 import com.example.kotlinmongo.clazz.field
 import com.example.kotlinmongo.collection.Author
+import com.example.kotlinmongo.collection.Book
 import com.example.kotlinmongo.collection.Status
 import com.example.kotlinmongo.extension.*
 import io.kotest.core.spec.style.StringSpec
@@ -26,25 +27,42 @@ class IdTest(
             )
         )
 
-        val document = document {
-            field(Author::id) eq author.id
-        } order {
-            field(Author::age) by DESC
-            field(Author::weight) by ASC
-        } group {
-            field(Author::status) and field(Author::age)
-        } sum {
-            field(Author::age) type Double::class alias "total"
-        } average {
-            field(Author::weight) alias "avg"
-        } max {
-            field(Author::height) alias "max"
-        } min {
-            field(Author::height) alias "min"
-        }
+        val document =
+            document {
+                field(Author::age) eq 30
+                embeddedDocument(Author::books).elemMatch {
+                    field(Book::price) exists false
+                    field(Book::description) startsWith "test"
+                }
+            } order {
+                field(Author::age) by DESC
+                field(Author::weight) by ASC
+            } group {
+                field(Author::status) and field(Author::age)
+            } sum {
+                field(Author::age) type Double::class alias "total"
+            } average {
+                field(Author::weight) alias "avg"
+            } max {
+                field(Author::height) alias "max"
+            } min {
+                field(Author::height) alias "min"
+            }
 
-        val map = mongoTemplate.aggregate(document, Author::class)
-        println("map: $map")
+        val result = mongoTemplate.aggregate(document, Author::class)
+
+        result.forEach {
+            val map = it["_id"] as Map<*, *>
+            map.forEach { (key, value) ->
+                println("$key = $value")
+            }
+
+            println(it["_id"])
+            println(it["total"])
+            println(it["avg"])
+            println(it["max"])
+            println(it["min"])
+        }
     }
 
     "id 값으로 조회2" {
