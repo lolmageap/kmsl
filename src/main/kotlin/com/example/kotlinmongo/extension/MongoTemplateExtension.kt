5ec1a5e7315f83bb904fa21d28b1c9f6f2edbb1e
lolmageap <ekxk1234@naver.com>
2024-09-08 14:33:31 +0900

    add simple group by function

--- src/main/kotlin/com/example/kotlinmongo/extension/MongoTemplateExtension.kt
+++ src/main/kotlin/com/example/kotlinmongo/extension/MongoTemplateExtension.kt
@@ -1,7 +1,6 @@
 package com.example.kotlinmongo.extension
 
-import com.example.kotlinmongo.clazz.EmptyGroup
-import com.example.kotlinmongo.clazz.Group
+import com.example.kotlinmongo.clazz.*
 import org.springframework.data.annotation.Id
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
@@ -10,6 +9,7 @@ import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.mapping.Field
 import org.springframework.data.mongodb.core.query.BasicQuery
 import kotlin.reflect.KClass
+import kotlin.reflect.KProperty1
 
 fun <T : Any> MongoTemplate.find(
     query: BasicQuery,
@@ -97,6 +97,262 @@ fun <T : Any> MongoTemplate.aggregate(
     ).uniqueMappedResult!!.map {
         it.key.toString() to it.value
     }.toMap()
+
+inline fun <reified T : Any, reified R : Any> MongoTemplate.sum(
+    query: BasicQuery,
+    property: KProperty1<T, R>,
+    alias: String = "total",
+): R {
+    val sumOfAll = query sum { field(property) alias alias }
+
+    return aggregate(sumOfAll.toAggregation(), T::class.java, Map::class.java)
+        .uniqueMappedResult?.let { result ->
+            result[alias] as? R
+                ?: throw TypeCastException("null cannot be cast to non-null type ${property.returnType}")
+        }
+        ?: throw NoSuchElementException("No element found")
+}
+
+inline fun <reified T : Any, reified R : Any, reified C : Any> MongoTemplate.sum(
+    query: BasicQuery,
+    property: KProperty1<T, R>,
+    castType: KClass<C>,
+    alias: String = "total",
+): C {
+    val sumOfAll = query sum { field(property) type castType alias alias }
+
+    return aggregate(sumOfAll.toAggregation(), T::class.java, Map::class.java)
+        .uniqueMappedResult?.let { result ->
+            result[alias].cast<C>()
+                ?: throw TypeCastException("null cannot be cast to non-null type ${property.returnType}")
+        }
+        ?: throw NoSuchElementException("No element found")
+}
+
+inline fun <reified T : Any, reified R : Any, reified K : Any> MongoTemplate.sum(
+    group: Group<T, K>,
+    property: KProperty1<T, R>,
+    alias: String = "total",
+): Map<K, R> {
+    val sumOfGroup = group sum { field(property) alias alias }
+
+    return aggregate(sumOfGroup.toAggregation(), T::class.java, Map::class.java)
+        .mappedResults.associate { result ->
+            val key = castIfEnum<K, T>(result, T::class)
+            val value = result[alias] as? R
+                ?: throw TypeCastException("null cannot be cast to non-null type ${property.returnType}")
+            key to value
+        }
+}
+
+inline fun <reified T : Any, reified R : Any, reified K : Any, reified C : Any> MongoTemplate.sum(
+    group: Group<T, K>,
+    property: KProperty1<T, R>,
+    castType: KClass<C>,
+    alias: String = "total",
+): Map<K, C> {
+    val sumOfGroup = group sum { field(property) type castType alias alias }
+
+    return aggregate(sumOfGroup.toAggregation(), T::class.java, Map::class.java)
+        .mappedResults.associate { result ->
+            val key = castIfEnum<K, T>(result, T::class)
+            val value = result[alias].cast<C>()
+                ?: throw TypeCastException("null cannot be cast to non-null type ${property.returnType}")
+            key to value
+        }
+}
+
+inline fun <reified T : Any, reified R : Any> MongoTemplate.average(
+    query: BasicQuery,
+    property: KProperty1<T, R>,
+    alias: String = "average",
+): Double {
+    val averageOfAll = query average { field(property) alias alias }
+
+    return aggregate(averageOfAll.toAggregation(), T::class.java, Map::class.java)
+        .uniqueMappedResult?.let { result ->
+            result[alias] as? Double
+                ?: throw TypeCastException("null cannot be cast to non-null type ${property.returnType}")
+        }
+        ?: throw NoSuchElementException("No element found")
+}
+
+inline fun <reified T : Any, reified R : Any, reified C : Any> MongoTemplate.average(
+    query: BasicQuery,
+    property: KProperty1<T, R>,
+    castType: KClass<C>,
+    alias: String = "average",
+): C {
+    val averageOfAll = query average { field(property) type castType alias alias }
+
+    return aggregate(averageOfAll.toAggregation(), T::class.java, Map::class.java)
+        .uniqueMappedResult?.let { result ->
+            result[alias].cast<C>()
+                ?: throw TypeCastException("null cannot be cast to non-null type ${property.returnType}")
+        }
+        ?: throw NoSuchElementException("No element found")
+}
+
+inline fun <reified T : Any, reified R : Any, reified K : Any> MongoTemplate.average(
+    group: Group<T, K>,
+    property: KProperty1<T, R>,
+    alias: String = "average",
+): Map<K, Double> {
+    val averageOfGroup = group average { field(property) alias alias }
+
+    return aggregate(averageOfGroup.toAggregation(), T::class.java, Map::class.java)
+        .mappedResults.associate { result ->
+            val key = castIfEnum<K, T>(result, T::class)
+            val value = result[alias] as? Double
+                ?: throw TypeCastException("null cannot be cast to non-null type ${property.returnType}")
+            key to value
+        }
+}
+
+inline fun <reified T : Any, reified R : Any, reified K : Any, reified C : Any> MongoTemplate.average(
+    group: Group<T, K>,
+    property: KProperty1<T, R>,
+    castType: KClass<C>,
+    alias: String = "average",
+): Map<K, C> {
+    val averageOfGroup = group average { field(property) type castType alias alias }
+
+    return aggregate(averageOfGroup.toAggregation(), T::class.java, Map::class.java)
+        .mappedResults.associate { result ->
+            val key = castIfEnum<K, T>(result, T::class)
+            val value = result[alias].cast<C>()
+                ?: throw TypeCastException("null cannot be cast to non-null type ${property.returnType}")
+            key to value
+        }
+}
+
+inline fun <reified T : Any, reified R : Any> MongoTemplate.max(
+    query: BasicQuery,
+    property: KProperty1<T, R>,
+    alias: String = "max",
+): R {
+    val maxOfAll = query max { field(property) alias alias }
+
+    return aggregate(maxOfAll.toAggregation(), T::class.java, Map::class.java)
+        .uniqueMappedResult?.let { result ->
+            result[alias] as? R
+                ?: throw TypeCastException("null cannot be cast to non-null type ${property.returnType}")
+        }
+        ?: throw NoSuchElementException("No element found")
+}
+
+inline fun <reified T : Any, reified R : Any, reified C : Any> MongoTemplate.max(
+    query: BasicQuery,
+    property: KProperty1<T, R>,
+    castType: KClass<C>,
+    alias: String = "max",
+): C {
+    val maxOfAll = query max { field(property) type castType alias alias }
+
+    return aggregate(maxOfAll.toAggregation(), T::class.java, Map::class.java)
+        .uniqueMappedResult?.let { result ->
+            result[alias].cast<C>()
+                ?: throw TypeCastException("null cannot be cast to non-null type ${property.returnType}")
+        }
+        ?: throw NoSuchElementException("No element found")
+}
+
+inline fun <reified T : Any, reified R : Any, reified K : Any> MongoTemplate.max(
+    group: Group<T, K>,
+    property: KProperty1<T, R>,
+    alias: String = "max",
+): Map<K, R> {
+    val maxOfGroup = group max { field(property) alias alias }
+
+    return aggregate(maxOfGroup.toAggregation(), T::class.java, Map::class.java)
+        .mappedResults.associate { result ->
+            val key = castIfEnum<K, T>(result, T::class)
+            val value = result[alias] as? R
+                ?: throw TypeCastException("null cannot be cast to non-null type ${property.returnType}")
+            key to value
+        }
+}
+
+inline fun <reified T : Any, reified R : Any, reified K : Any, reified C : Any> MongoTemplate.max(
+    group: Group<T, K>,
+    property: KProperty1<T, R>,
+    castType: KClass<C>,
+    alias: String = "max",
+): Map<K, C> {
+    val maxOfGroup = group max { field(property) type castType alias alias }
+
+    return aggregate(maxOfGroup.toAggregation(), T::class.java, Map::class.java)
+        .mappedResults.associate { result ->
+            val key = castIfEnum<K, T>(result, T::class)
+            val value = result[alias].cast<C>()
+                ?: throw TypeCastException("null cannot be cast to non-null type ${property.returnType}")
+            key to value
+        }
+}
+
+inline fun <reified T : Any, reified R : Any> MongoTemplate.min(
+    query: BasicQuery,
+    property: KProperty1<T, R>,
+    alias: String = "min",
+): R {
+    val minOfAll = query min { field(property) alias alias }
+
+    return aggregate(minOfAll.toAggregation(), T::class.java, Map::class.java)
+        .uniqueMappedResult?.let { result ->
+            result[alias] as? R
+                ?: throw TypeCastException("null cannot be cast to non-null type ${property.returnType}")
+        }
+        ?: throw NoSuchElementException("No element found")
+}
+
+inline fun <reified T : Any, reified R : Any, reified C : Any> MongoTemplate.min(
+    query: BasicQuery,
+    property: KProperty1<T, R>,
+    castType: KClass<C>,
+    alias: String = "min",
+): C {
+    val minOfAll = query min { field(property) type castType alias alias }
+
+    return aggregate(minOfAll.toAggregation(), T::class.java, Map::class.java)
+        .uniqueMappedResult?.let { result ->
+            result[alias].cast<C>()
+                ?: throw TypeCastException("null cannot be cast to non-null type ${property.returnType}")
+        }
+        ?: throw NoSuchElementException("No element found")
+}
+
+inline fun <reified T : Any, reified R : Any, reified K : Any> MongoTemplate.min(
+    group: Group<T, K>,
+    property: KProperty1<T, R>,
+    alias: String = "min",
+): Map<K, R> {
+    val minOfGroup = group min { field(property) alias alias }
+
+    return aggregate(minOfGroup.toAggregation(), T::class.java, Map::class.java)
+        .mappedResults.associate { result ->
+            val key = castIfEnum<K, T>(result, T::class)
+            val value = result[alias] as? R
+                ?: throw TypeCastException("null cannot be cast to non-null type ${property.returnType}")
+            key to value
+        }
+}
+
+inline fun <reified T : Any, reified R : Any, reified K : Any, reified C : Any> MongoTemplate.min(
+    group: Group<T, K>,
+    property: KProperty1<T, R>,
+    castType: KClass<C>,
+    alias: String = "min",
+): Map<K, C> {
+    val minOfGroup = group min { field(property) type castType alias alias }
+
+    return aggregate(minOfGroup.toAggregation(), T::class.java, Map::class.java)
+        .mappedResults.associate { result ->
+            val key = castIfEnum<K, T>(result, T::class)
+            val value = result[alias].cast<C>()
+                ?: throw TypeCastException("null cannot be cast to non-null type ${property.returnType}")
+            key to value
+        }
+}
 
 val KClass<*>.fieldName
     get() = this.java.declaredFields.first { it.isAnnotationPresent(Id::class.java) }
