b3f509b1fe8e1f1391d3644423acafb10b560e80
lolmageap <ekxk1234@naver.com>
2024-09-12 23:12:41 +0900

    add field name constant

---
+++ src/main/kotlin/com/example/kotlinmongo/clazz/FieldName.kt
@@ -0,0 +1,10 @@
+package com.example.kotlinmongo.clazz
+
+object FieldName {
+    const val ID = "_id"
+    const val SUM_FIELD = "sumField"
+    const val AVERAGE_FIELD = "averageField"
+    const val MIN_FIELD = "minField"
+    const val MAX_FIELD = "maxField"
+    const val COUNT_FIELD = "countField"
+}
