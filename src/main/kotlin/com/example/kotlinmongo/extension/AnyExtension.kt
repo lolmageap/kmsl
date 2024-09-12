4ceaf320ec59bd9f7772e090771101838929a1a9
lolmageap <ekxk1234@naver.com>
2024-09-12 23:10:26 +0900

    add any extension method

---
+++ src/main/kotlin/com/example/kotlinmongo/extension/AnyExtension.kt
@@ -0,0 +1,9 @@
+package com.example.kotlinmongo.extension
+
+import java.math.BigDecimal
+
+fun Any?.toLong(): Long = this.toString().toLong()
+fun Any?.toInt(): Int = this.toString().toInt()
+fun Any?.toDouble(): Double = this.toString().toDouble()
+fun Any?.toFloat(): Float = this.toString().toFloat()
+fun Any?.toBigDecimal(): BigDecimal = this.toString().toBigDecimalOrNull() ?: BigDecimal.ZERO
