5803ca9734ce5c6e5918f7feb27c01a65f6405dd
lolmageap <ekxk1234@naver.com>
2024-09-14 20:23:38 +0900

    create embedded documents class

--- src/main/kotlin/com/example/kotlinmongo/clazz/EmbeddedDocument.kt
+++ src/main/kotlin/com/example/kotlinmongo/clazz/EmbeddedDocument.kt
@@ -3,15 +3,28 @@ package com.example.kotlinmongo.clazz
 import org.springframework.data.mapping.toDotPath
 import kotlin.reflect.KProperty1
 
-class EmbeddedDocument private constructor(
-    private val property: KProperty1<*, *>,
+class EmbeddedDocument<T, R> private constructor(
+    private val property: KProperty1<T, R>,
 ) {
     val name: String
         get() = property.toDotPath()
 
     companion object {
-        fun of(
-            property: KProperty1<*, *>,
+        fun<T, R> of(
+            property: KProperty1<T, R>,
         ) = EmbeddedDocument(property)
+    }
+}
+
+class EmbeddedDocuments<T, R> private constructor(
+    private val property: KProperty1<T, List<R>>,
+) {
+    val name: String
+        get() = property.toDotPath()
+
+    companion object {
+        fun<T, R> of(
+            property: KProperty1<T, List<R>>,
+        ) = EmbeddedDocuments(property)
     }
 }
