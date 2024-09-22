748d20e1280e85bddfb0832c92de920133e56056
lolmageap <ekxk1234@naver.com>
2024-09-22 16:14:24 +0900

    configuring jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,15 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.2.4"
-	id("io.spring.dependency-management") version "1.1.4"
 	kotlin("jvm") version "1.9.23"
 	kotlin("plugin.spring") version "1.9.23"
+	id("maven-publish")
+	id("org.springframework.boot") version "3.2.4"
+	id("io.spring.dependency-management") version "1.1.4"
 }
 
-group = "com.example"
-version = "0.0.1-SNAPSHOT"
+group = "cherhy.mongo"
+version = "0.0.1"
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_17
@@ -20,7 +21,6 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 
@@ -29,6 +29,17 @@ dependencies {
 	testImplementation("io.kotest:kotest-assertions-core:5.7.2")
 	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
 	testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:2.0.2")
+}
+
+publishing {
+	publications {
+		create<MavenPublication>("mavenJava") {
+			from(components["java"])
+			groupId = project.group.toString()
+			artifactId = project.name
+			version = project.version.toString()
+		}
+	}
 }
 
 tasks.withType<KotlinCompile> {
