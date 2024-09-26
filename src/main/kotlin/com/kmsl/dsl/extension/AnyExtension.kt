20433d03f8370f235a9d2adc89f0d83ce7ba0659
lolmageap <ekxk1234@naver.com>
2024-09-26 22:59:57 +0900

    add internal keyword

--- src/main/kotlin/com/kmsl/dsl/extension/AnyExtension.kt
+++ src/main/kotlin/com/kmsl/dsl/extension/AnyExtension.kt
@@ -2,8 +2,8 @@ package com.kmsl.dsl.extension
 
 import java.math.BigDecimal
 
-fun Any?.toLong(): Long = this.toString().toLong()
-fun Any?.toInt(): Int = this.toString().toInt()
-fun Any?.toDouble(): Double = this.toString().toDouble()
-fun Any?.toFloat(): Float = this.toString().toFloat()
-fun Any?.toBigDecimal(): BigDecimal = this.toString().toBigDecimalOrNull() ?: BigDecimal.ZERO
+internal fun Any?.toLong(): Long = this.toString().toLong()
+internal fun Any?.toInt(): Int = this.toString().toInt()
+internal fun Any?.toDouble(): Double = this.toString().toDouble()
+internal fun Any?.toFloat(): Float = this.toString().toFloat()
+internal fun Any?.toBigDecimal(): BigDecimal = this.toString().toBigDecimalOrNull() ?: BigDecimal.ZERO
