0d6b81031cd6d857ffd0fa671459b2b7bb11d35d
lolmageap <ekxk1234@naver.com>
2024-10-01 13:30:12 +0900

    remove version tag

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,14 +22,14 @@ repositories {
 }
 
 dependencies {
-	implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb:3.3.4")
+	implementation("jakarta.persistence:jakarta.persistence-api")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
-	testImplementation("io.kotest:kotest-assertions-core:5.7.2")
-	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+	testImplementation("io.kotest:kotest-runner-junit5")
+	testImplementation("io.kotest:kotest-assertions-core")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring")
 }
 
 publishing {
