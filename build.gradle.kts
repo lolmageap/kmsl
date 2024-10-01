c68595bd587f6df639211ca22ff76d7be6a8b95a
lolmageap <ekxk1234@naver.com>
2024-10-01 12:58:11 +0900

    add jakarta persistence api dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,8 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb:3.3.4")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
