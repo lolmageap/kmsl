7f56d56f0aad9c7f84dfb1588a5925bc1b64c2e7
lolmageap <ekxk1234@naver.com>
2024-10-03 15:56:43 +0900

    add property and get join class info function

--- src/main/kotlin/com/kmsl/dsl/clazz/Lookup.kt
+++ src/main/kotlin/com/kmsl/dsl/clazz/Lookup.kt
@@ -1,6 +1,9 @@
 package com.kmsl.dsl.clazz
 
+import com.kmsl.dsl.extension.fieldName
 import org.bson.Document
+import kotlin.reflect.KClass
+import kotlin.reflect.full.findAnnotations
 
 data class Lookup(
     val from: String,
@@ -8,4 +11,59 @@ data class Lookup(
     val foreignField: String,
     val alias: String,
     val matchDocument: Document,
-)
+    val joinedClasses: List<KClass<*>> = emptyList(),
+) {
+    val joinedClassesNonDuplicatedFieldNames: Array<String>
+        get() =
+            joinedClasses
+                .map { clazz ->
+                    clazz.java.declaredFields.mapNotNull { field ->
+                        if (field.name != COMPANION) field.fieldName else null
+                    }
+                }
+                .flatten()
+                .groupingBy { it }
+                .eachCount()
+                .filter { it.value == 1 }
+                .keys
+                .toList()
+                .toTypedArray()
+
+    val duplicatedFieldNames
+        get() =
+            joinedClasses
+                .map { clazz ->
+                    clazz.java.declaredFields.mapNotNull { field ->
+                        if (field.name != COMPANION) field.fieldName else null
+                    }
+                }
+                .flatten()
+                .groupingBy { it }
+                .eachCount()
+                .filter { it.value > 1 }
+                .keys
+                .toList()
+
+    val firstClassName
+        get() = joinedClasses.first().className()
+
+    val lastClassName
+        get() = joinedClasses.last().className()
+
+    private fun KClass<*>.className() =
+        this.findAnnotations<org.springframework.data.mongodb.core.mapping.Document>().first().collection
+
+    private fun List<String>.mangling(): Array<String> {
+        val fieldCountMap = mutableMapOf<String, Int>()
+
+        return this.map { fieldName ->
+            val count = fieldCountMap.getOrDefault(fieldName, 0)
+            fieldCountMap[fieldName] = count + 1
+            if (count == 0) fieldName else "${fieldName}_$count"
+        }.toTypedArray()
+    }
+
+    companion object {
+        const val COMPANION = "Companion"
+    }
+}
