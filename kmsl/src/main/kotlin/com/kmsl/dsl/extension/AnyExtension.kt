ac273a78dfdbf80f619d86858824e21225a88245
lolmageap <ekxk1234@naver.com>
2024-11-06 22:19:40 +0900

    refactor extension functions to convert type

--- kmsl/src/main/kotlin/com/kmsl/dsl/extension/AnyExtension.kt
+++ kmsl/src/main/kotlin/com/kmsl/dsl/extension/AnyExtension.kt
@@ -2,9 +2,9 @@ package com.kmsl.dsl.extension
 
 import java.math.BigDecimal
 
-internal fun Any?.toLong(): Long = this.toString().toLong()
-internal fun Any?.toInt(): Int = this.toString().toInt()
-internal fun Any?.toDouble(): Double = this.toString().toDouble()
-internal fun Any?.toFloat(): Float = this.toString().toFloat()
-internal fun Any?.toBigDecimal(): BigDecimal = this.toString().toBigDecimalOrNull() ?: BigDecimal.ZERO
+internal fun Any?.toLong(): Long = this?.toString()?.replaceAfter(".", "")?.toLongOrNull() ?: 0
+internal fun Any?.toInt(): Int = this?.toString()?.replaceAfter(".", "")?.toIntOrNull() ?: 0
+internal fun Any?.toDouble(): Double = this?.toString()?.toDoubleOrNull() ?: 0.0
+internal fun Any?.toFloat(): Float = this?.toString()?.toFloatOrNull() ?: 0.0f
+internal fun Any?.toBigDecimal(): BigDecimal = this?.toString()?.toBigDecimalOrNull() ?: BigDecimal.ZERO
 internal val Any?.noReturn: Unit get() = Unit
